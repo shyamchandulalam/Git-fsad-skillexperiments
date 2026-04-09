38570cc1aac213d17ed7f9d0b94dc87004fff8f4
LALAM SHYAM CHANDU <2400031579@kluniversity.in>
2026-04-09 22:30:24 +0530

    Delete skill12 directory

--- skill12/backend/src/main/java/com/example/student/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.student.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.student.model.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
